92cb8302fd8f45bf94e7fbc1c3d3693a45d868ac
tamta123 <tamtag186@gmail.com>
2023-07-20 19:41:47 +0400

    update moon and arrow icons for dark and light modes

--- src/views/CountryCard/CountryCard.jsx
+++ src/views/CountryCard/CountryCard.jsx
@@ -32,14 +32,45 @@ function CountryCard({ countries, mode }) {
         style={{ boxShadow: "0px 0px 7px 0px rgba(0, 0, 0, 0.29)" }}
         onClick={handleBack}
       >
-        <img
-          src={
-            mode === "dark"
-              ? "../../../public/arrow-white.svg"
-              : "../../../public/arrow.svg"
-          }
-          alt="arrow"
-        />
+        <div>
+          {mode === "dark" ? (
+            <svg
+              width="18"
+              height="18"
+              viewBox="0 0 18 18"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <g id="call-made">
+                <path
+                  id="Shape"
+                  fill-rule="evenodd"
+                  clip-rule="evenodd"
+                  d="M5.81802 3.6967L6.87868 4.75736L3.3785 8.25754H16.7428L16.7428 9.74246H3.3785L6.87868 13.2426L5.81802 14.3033L0.514719 9L5.81802 3.6967Z"
+                  fill="white"
+                />
+              </g>
+            </svg>
+          ) : (
+            <svg
+              width="18"
+              height="18"
+              viewBox="0 0 18 18"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <g id="call-made">
+                <path
+                  id="Shape"
+                  fill-rule="evenodd"
+                  clip-rule="evenodd"
+                  d="M5.81802 3.6967L6.87868 4.75736L3.3785 8.25754H16.7428L16.7428 9.74246H3.3785L6.87868 13.2426L5.81802 14.3033L0.514719 9L5.81802 3.6967Z"
+                  fill="#111517"
+                />
+              </g>
+            </svg>
+          )}
+        </div>
         Back
       </button>
       <div className="mt-[64px] w-[85%] lg:flex xl:gap-[145px] xl:justify-between xl:items-center xl:mt-[20] lg:w-[95%]">
